52dd58196bbe89842833ca9c9a50a88b3d9bb0b5
Matyas Hybl <hyma.code@gmail.com>
2026-03-05 10:22:27 +0100

    add config.h

---
+++ config.h
@@ -0,0 +1,9 @@
+// Copyright 2024 QMK
+// SPDX-License-Identifier: GPL-2.0-or-later
+#pragma once
+
+#define TAPPING_TERM 200
+#define ENABLE_COMPILE_KEYCODE
+#define QUICK_TAP_TERM 0
+#define BOTH_SHIFTS_TURNS_ON_CAPS_WORD
+#define EE_HANDS
